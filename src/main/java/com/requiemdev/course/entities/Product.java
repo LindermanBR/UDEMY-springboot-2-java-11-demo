a3258c055cb5138fc185fca9b0697c5103630cb3
LindermanBR <linderman.moura@outlook.com>
2022-06-08 17:11:08 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/requiemdev/course/entities/Product.java
+++ src/main/java/com/requiemdev/course/entities/Product.java
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -32,6 +35,10 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name = "produc_id"),// indica qual o nome da chave estrangeira referente a tabela de produtos (essa entidade)
 	inverseJoinColumns = @JoinColumn(name = "category_id"))// define qual a chave estrangeira da outra entidade (no caso a category)
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Product() {
 		
@@ -90,6 +97,15 @@ public class Product implements Serializable {
 		return categories;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
